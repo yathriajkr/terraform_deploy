719abd934b136882531c57a0070a59761d3eae26
yathriajkr <144806369+yathriajkr@users.noreply.github.com>
2025-01-22 10:27:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "aws" {
     region = "us-west-2"
-    access_key = 
-    secret_key = 
+    access_key = {{ AWS_ACCESS_KEY_ID }}
+    secret_key = {{ AWS_SECRET_ACCESS_KEY }}
 }
